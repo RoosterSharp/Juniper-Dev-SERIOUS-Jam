bc28846e5021f7e015241dd7546fa3c6529495aa
Authentic <skylerbaron1997@gmail.com>
2026-06-20 23:15:05 -0500

    Cylinder animation + buttons
    - Added serious man
    - Added cylinder animation
    - Added button detection

--- scenes/cylinder.gd
+++ scenes/cylinder.gd
@@ -1,20 +1,8 @@
 extends Node2D
 
-@onready var sprite: Sprite2D = $Sprite2D
+func _ready():
+	for button in $Buttons.get_children():
+		button.pressed.connect(button_pressed.bind(button.get_meta("num")))
 
-const spin_frequency := 1 #frames
-var spin_timer := 0
-const spin_amount := 15 #degrees
-
-func _process(delta):
-	tick()
-	
-func tick():
-	if spin_timer >= 0:
-		spin_timer -= 1
-	else:
-		rotate_sprite()
-		spin_timer = spin_frequency
-
-func rotate_sprite():
-	sprite.rotate(deg_to_rad(spin_amount))
+func button_pressed(num: int):
+	print("pressed " + str(num))
