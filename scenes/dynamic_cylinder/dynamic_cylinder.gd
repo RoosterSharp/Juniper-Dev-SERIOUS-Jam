52038b0cddc3fe3c79e0e48a94d0665b2c12e488
RoosterSharp <roostergaming2006@gmail.com>
2026-06-20 23:40:46 -0500

    dynamic cylinder sprite

---
+++ scenes/dynamic_cylinder/dynamic_cylinder.gd
@@ -0,0 +1,53 @@
+@tool
+extends Node2D
+
+const DIVIT_DIST = 65
+const SLOT_DIST = -40
+const SLOT_WIDTH = 70
+
+@export var num_slots := 6:
+	set(value):
+		if Engine.is_editor_hint():
+			num_slots = max(value,6)
+			_update()
+@export var cylinder_tex : GradientTexture2D
+@export var divit_tex : Texture2D
+@export var slot_tex : Texture2D
+
+
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+
+func _ready() -> void:
+	set_num_slots(num_slots)
+
+func set_num_slots(num):
+	num_slots = max(num,6)
+	_update()
+
+func _update():
+	for child in canvas_group.get_children():
+		child.queue_free()
+	
+	var radius = ((SLOT_WIDTH*num_slots/PI)+SLOT_WIDTH)/2
+	
+	cylinder_tex.height = radius*2
+	cylinder_tex.width = radius*2
+	
+	var cylinder_spr = Sprite2D.new()
+	cylinder_spr.texture = cylinder_tex
+	canvas_group.add_child(cylinder_spr)
+	for slot_idx in num_slots:
+		
+		var new_slot = Sprite2D.new()
+		canvas_group.add_child(new_slot)
+		new_slot.texture = slot_tex
+		new_slot.position = Vector2.UP.rotated(2*PI/num_slots*slot_idx)*(radius+SLOT_DIST)
+		
+		var new_divit = Sprite2D.new()
+		canvas_group.add_child(new_divit)
+		new_divit.texture = divit_tex
+		
+		
+		var slot_width = 2*PI/num_slots
+		new_divit.position = Vector2.UP.rotated(2*PI/num_slots*slot_idx+slot_width/2)*(radius+DIVIT_DIST)
+		
