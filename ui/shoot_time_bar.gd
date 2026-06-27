decff68362d71616ee99b7d5fdc3eb805b71c136
Authentic <skylerbaron1997@gmail.com>
2026-06-27 00:16:13 -0500

    Button sprites

--- ui/shoot_time_bar.gd
+++ ui/shoot_time_bar.gd
@@ -1,5 +1,5 @@
 extends ProgressBar
 
-func _process(delta):
+func _process(_delta):
 	max_value = Cylinder.get_instance().shoot_timer.wait_time
 	value = Cylinder.get_instance().shoot_timer.time_left
